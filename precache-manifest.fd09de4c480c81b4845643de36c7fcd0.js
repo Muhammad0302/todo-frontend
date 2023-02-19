cc553533cc80b7c402ba6a1272da34943ab58bc7
@mi477048 <mi477048@gmail.com>
2023-02-19 21:01:17 +0500

    Updates

---
+++ precache-manifest.fd09de4c480c81b4845643de36c7fcd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39f43d05fb4f935ce2687a8ecbfb6dc",
+    "url": "/todo-frontend/index.html"
+  },
+  {
+    "revision": "773cb283f56479d9257a",
+    "url": "/todo-frontend/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7eaff49f1d1fd94982c0",
+    "url": "/todo-frontend/static/css/main.5db8bfe3.chunk.css"
+  },
+  {
+    "revision": "773cb283f56479d9257a",
+    "url": "/todo-frontend/static/js/2.44f09929.chunk.js"
+  },
+  {
+    "revision": "7eaff49f1d1fd94982c0",
+    "url": "/todo-frontend/static/js/main.57bb08e1.chunk.js"
+  },
+  {
+    "revision": "76396b2f8dfbe8e93026",
+    "url": "/todo-frontend/static/js/runtime~main.130a5b63.js"
+  }
+]);
