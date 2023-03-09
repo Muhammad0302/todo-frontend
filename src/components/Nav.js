424f67ffc689516ab08b9a8e3ee2447bc1e9124f
@mi477048 <mi477048@gmail.com>
2023-03-10 00:38:24 +0500

    .yml file changed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Nav() {
+	// comment goes here
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3 header">
 			<Link className="navbar-brand" to="/">
